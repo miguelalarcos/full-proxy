79140347073de93d13b7bfa05936f53b4892ec95
Miguel Ángel Alarcos Torrecillas <miguel.alarcos@gmail.com>
2019-06-03 23:34:24 +0200

    development framework prepared

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
       proxy: {
         '/api/*': {
-            target: 'http://api:8888',
+            target: 'http://api:3000',
             changeOrigin: true,
             //logLevel: 'debug',
         }
